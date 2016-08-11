2e3251c2a9b7ada2a6ba8daf9ce6e37edbbfc70f
ljtale <ljtale@gmail.com>
2016-08-11 16:58:29 -0500

    add skeleton

---
+++ drivers/ljtale/universal-pm.c
@@ -0,0 +1,6 @@
+/**
+ * universal system-wide PM source file @ljtale
+ */
+
+#include <linux/universal-utils.h>
+#include <linux/universal-pm.h>
---
+++ include/linux/universal-pm.h
@@ -0,0 +1,19 @@
+#ifndef _LINUX_UNIVERSAL_PM_H
+#define _LINUX_UNIVERSAL_PM_H
+
+#include <linux/kernel.h>
+#include <linux/platform_device.h>
+#include <linux/regmap.h>
+#include <linux/of_device.h>
+#include <linux/interrupt.h>
+#include <linux/dmaengine.h>
+#include <linux/omap-dmaengine.h>
+#include <linux/pm_runtime.h>
+
+#include <linux/i2c.h>
+#include <linux/regulator/driver.h>
+#include <linux/regulator/machine.h>
+#include <linux/platform_data/at24.h>
+
+
+#endif /* _LINUX_UNIVERSAL_RPM_H */
